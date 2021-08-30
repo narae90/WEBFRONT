85c7ba3b6d3cc2743123b459893c61b98570168a
김나래 <narae900@naver.com>
2021-08-30 15:47:14 +0900

    배열 확장 메소드

---
+++ JS/12-array-extended.js
@@ -0,0 +1,50 @@
+function testForEach(){
+    //* forEach 메소드 : 배열의 요소를 하나씩 콜백에 전달
+    let source = ["Banana", "Orange", "Apple", "Mango"];
+    console.log("======forEach");
+    source.forEach(items => {
+        //* 콜백 함수의 인자가 1개 : 개별 요소
+        console.log(items);
+    });
+    source.forEach((items, index) => {
+        //* 콜백 함수의 인자가 2개 : 개별 요소, 배열 내의 인덱스
+        console.log(`${index}번째 요소의 ${items}`);
+    })
+    source.forEach((item, index, arr) => {
+        //* 콜백 함수의 인자가 3개 : 개별요소, 인덱스, 배열 자체
+        console.log(`배열 ${arr}의 ${index}번째 요소의 ${item}`);
+    })
+}
+// testForEach();
+
+function testSome(){
+    let data = [
+        { name : "홍길동", age: 28},
+        { name : "장길산", age: 35},
+        { name : "전우치", age: 25},
+        
+    ]
+    //* some, any : 조건을 만족하는 데이터가 1개 이상 있을 때
+    let result = data.some(x => {
+        return x.age > 25;
+    })
+
+    console.log("----------------some : age > 25 ? ", result);
+}
+
+// testSome();
+
+function testEvery(){
+    let data = [
+        { name : "홍길동", age: 28},
+        { name : "장길산", age: 35},
+        { name : "전우치", age: 25},
+    ]
+
+    //* every : 모든 요소가 콜백 함수의 조건을 만족시킬 때 true
+    let result = data.every(x => {
+        return x.age >25;
+    });
+    console.log("----------------every : age > 25 ? ", result);
+}
+testEvery();
