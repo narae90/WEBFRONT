5dd743ae997f6ec6afdf5ed20ee985f624718dec
김나래 <narae900@naver.com>
2021-08-31 14:27:39 +0900

    DOM 요소 찾기,속성 읽기/쓰기

---
+++ ex02-js/script/print.js
@@ -0,0 +1,10 @@
+
+if(name === null){
+    name = "Anonumous";
+}
+document.writeln("<p>Welcome," + name + "</p>");
+let result = confirm ("준비 되셨습니까?"); // 사용자 선택
+if(result) {
+    alert("한번 실행해 봅시다.");
+
+}
